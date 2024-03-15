24e52c554f3cc9734b12af628ea098e88c08d108
CultDog <kabakv09@gamil.com>
2024-03-15 23:26:53 +0300

    events hotfix

--- src/components/events/events.jsx
+++ src/components/events/events.jsx
@@ -1,10 +1,11 @@
 import { Button, Typography, Breadcrumb, Card, Space, List } from 'antd';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 function Events() {
 	const [events , setEvents] = useState([]);
 	const loginUrl = "http://127.0.0.1:8000/event/events_with_nominations?offset=0&limit=10"
+
 	const GetEvents = async () =>{
 		const myHeaders = new Headers();
 		myHeaders.append("accept", "application/json");
@@ -17,41 +18,43 @@ function Events() {
 		};
 		try{
 		const response = await fetch(loginUrl, requestOptions);
-		const data = await response.json();
-		console.log(data);
-		setEvents(data)
+		setEvents(await response.json());
+		console.log(events);
 		}catch (error){
 			console.error(error);
 		}
 	}
 	
-		const EventsList = events.map((event, index) => {
-			return(
-				<Card 
-					key={index}
+	useEffect(() => {
+		GetEvents();
+	  }, []);
+
+	const EventsList = events.map((event, index) => {
+		return(
+			<Card 
+				key={index}
+				size="small"
+				title = {
+					<Space direction="vertical">
+						<Typography.Title level={2}>{event.name}</Typography.Title>
+						<Typography.Text type="secondary">{event.date}</Typography.Text>
+					</Space>
+				} 
+			>
+				<List
 					size="small"
-					title = {
-						<Space direction="vertical">
-							<Typography.Title level={2}>{event.name}</Typography.Title>
-							<Typography.Text type="secondary">{event.date}</Typography.Text>
-						</Space>
-					} 
-				>
-					<List
-						size="small"
-						header = {<Typography.Text>Компетенции: </Typography.Text>}
-						footer = {<Link to = {""}>Перейти к мероприятию</Link>}
-						dataSource={event.nominations}
-						renderItem={(item) => (
-							<List.Item>
-								{item}
-							</List.Item>
-						)}
-					/>
-				</Card>
-			)
-   })
-   GetEvents();
+					header = {<Typography.Text>Компетенции: </Typography.Text>}
+					footer = {<Link to = {""}>Перейти к мероприятию</Link>}
+					dataSource={event.nominations}
+					renderItem={(item) => (
+						<List.Item>
+							{item.name}
+						</List.Item>
+					)}
+				/>
+			</Card>
+		)
+	})
    const eventslist = EventsList;
    return (
 		<Space direction="vertical" size="large" style={{ display: 'flex' }}>
