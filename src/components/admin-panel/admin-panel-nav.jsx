7ae918098e9cc95cc6b9b650f6eaf81e1b618411
CultDog <kabakv09@gamil.com>
2024-04-03 16:03:48 +0300

    Tooltip

--- src/components/admin-panel/admin-panel-nav.jsx
+++ src/components/admin-panel/admin-panel-nav.jsx
@@ -32,36 +32,44 @@ function AdminPanelNav({role}) {
 				{
 					role === 'admin' && (
 						<li>
+							<Tooltip title="Управление участниками">
 							<NavLink
 								to="/admin/participants"
 								className="admin-panel__menu-link admin-panel__menu-link--add-participants"
 							/>
+							</Tooltip>
 						</li>
 					)
 				}
 				{
 					role === 'admin' && (
 						<li>
+							<Tooltip title="Управление пользователями">
 							<NavLink
 								to="/admin/users"
 								className="admin-panel__menu-link admin-panel__menu-link--users"
 							/>
+							</Tooltip>
 						</li>
 					)
 				}
 				<li>
+					<Tooltip title="Натсройки аккаунта">
 					<NavLink
 						to="/admin/settings"
 						className="admin-panel__menu-link admin-panel__menu-link--settings"
 					/>
+					</Tooltip>
 				</li>
 			</ul>
 			<ul className="admin-panel__menu-list">
 				<li>
+					<Tooltip title="Выход">
 					<NavLink
 						to="/admin/logout"
 						className="admin-panel__menu-link admin-panel__menu-link--logout"
 					/>
+					</Tooltip>
 				</li>
 			</ul>
 		</nav>
