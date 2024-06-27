d2cfd30a5758f94e2d5db01f559eedff27e9fa18
CultDog <kabakv09@gmail.com>
2024-06-27 13:35:28 +0300

    fix coments

--- src/UI/judgment/events/event-regulation.jsx
+++ src/UI/judgment/events/event-regulation.jsx
@@ -2,7 +2,27 @@ import { Typography, Upload, message, Button, Flex } from 'antd'
 import FormItem from 'antd/es/form/FormItem'
 import { UploadOutlined } from '@ant-design/icons';
 
+const UPLOADING = 'uploading';
+const DONE = 'done';
+const ERROR = 'error';
+
 function EventRegulation({ name }) {
+
+  const props = {
+    name : 'file',
+    action : 'https://660d2bd96ddfa2943b33731c.mockapi.io/api/upload',
+    onChange(info) {
+      if (info.file.status !== UPLOADING) {
+        console.log(info.file, info.fileList);
+      }
+      if (info.file.status === DONE) {
+        message.success(`${info.file.name} Файл загружен успешно`);
+      } else if (info.file.status === ERROR) {
+        message.error(`${info.file.name} Ошибка загрузки файла`);
+      }
+    },
+  };
+    
   return (
     <FormItem
       name={name}
@@ -11,21 +31,7 @@ function EventRegulation({ name }) {
     >
       <Flex gap="middle">
         <Typography.Text>Положение о проведении мероприятия: </Typography.Text>
-        <Upload 
-          name = 'file'
-          action = 'https://660d2bd96ddfa2943b33731c.mockapi.io/api/upload'
-          onChange= {(info) =>{
-              if (info.file.status !== 'Загрузка') {
-                console.log(info.file, info.fileList);
-              }
-              if (info.file.status === 'Готово') {
-                message.success(`${info.file.name} Файл успешно загружен`);
-              } else if (info.file.status === 'error') {
-                message.error(`${info.file.name} Ошибка загрузки файла`);
-              }
-            }
-          }
-        >
+        <Upload {...props} >
           <Button icon={<UploadOutlined />}>Нажмите что бы загрузить</Button>
         </Upload>
       </Flex>
